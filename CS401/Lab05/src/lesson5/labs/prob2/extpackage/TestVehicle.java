2b86b2777cf866a8983ccf6d98c0bde3093f8617
Sanjaya <117646925+jansjay@users.noreply.github.com>
2022-12-04 18:22:53 -0600

    Minor modification

--- CS401/Lab05/src/lesson5/labs/prob2/extpackage/TestVehicle.java
+++ CS401/Lab05/src/lesson5/labs/prob2/extpackage/TestVehicle.java
@@ -1,23 +1,20 @@
 package lesson5.labs.prob2.extpackage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lesson5.labs.prob2.Vehicle;
 import lesson5.labs.prob2.VehicleFactory;
 
 public class TestVehicle {
 
 	public static void main(String[] args) {
-		Vehicle v1 = VehicleFactory.getVehicle("bus");
-		v1.startEngine();
-		
-		Vehicle v2 = VehicleFactory.getVehicle("car");
-		v2.startEngine();
-		
-		Vehicle v3 = VehicleFactory.getVehicle("truck");
-		v3.startEngine();
-		
-		Vehicle v4 = VehicleFactory.getVehicle("electriccar");
-		v4.startEngine();
-		
+		String[] vehicleTypes = {"bus", "car", "truck", "electriccar"};
+		for(String s: vehicleTypes)
+		{
+			Vehicle vehicle = VehicleFactory.getVehicle(s);
+			vehicle.startEngine();
+		}		
 	}
 
 }
